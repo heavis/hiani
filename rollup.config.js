bda9a0fbc71ad5a980af0fba5011781c067259a1
heavi <308975917@qq.com>
2023-04-04 23:28:37 +0800

    切换版本

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/hiani.min.js',
+      format: 'iife',
+      name: 'MyLibrary',
+      sourcemap: true,
+      plugins: production ? [uglify()] : [],
+    },
+    {
+      file: 'dist/hiani.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/hiani.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json',
+    }),
+    nodeResolve(),
+    commonjs(),
+  ],
+  external: ['my-dependency'],
+};
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Adds two numbers.
+ * @function
+ * @param {number} x - The first number.
+ * @param {number} y - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+ export function add(x: number, y: number) {
+    return x + y;
+  }
